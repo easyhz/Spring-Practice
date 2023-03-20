239d4229f9d8f90a8dc1dfb5c3640f6328ce9cfc
EASYhz <jiyon3@gachon.ac.kr>
2023-03-20 21:02:24 +0900

    Update:

--- src/main/java/com/example/spring_practice/controller/ItemController.java
+++ src/main/java/com/example/spring_practice/controller/ItemController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class ItemController {
     private final ItemService itemService;
 
+    // RequestParam vs RequestBody
     @GetMapping(value = "/get") // http://localhost:8080/items/get
     public List<Items> getAllItems() {
         return itemService.getAllItems();
